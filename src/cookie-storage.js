d8e787b2a4819cd25eacc223b068078082cb9db1
bouzuya <m@bouzuya.net>
2015-03-22 10:06:32 +0900

    add Web Storage interface

--- src/cookie-storage.js
+++ src/cookie-storage.js
@@ -1,4 +1,28 @@
 class CookieStorage {
-};
+  // TODO
+  constructor() {
+    // define length property
+  }
+
+  // TODO
+  key(index) {
+  }
+
+  // TODO
+  getItem(key) {
+  }
+
+  // TODO
+  setItem(key, data) {
+  }
+
+  // TODO
+  removeItem(key) {
+  }
+
+  // TODO
+  clear() {
+  }
+}
 
 export { CookieStorage };
