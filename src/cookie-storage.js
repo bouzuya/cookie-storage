72f1b6b380758af97c38a5e98ce641926810c1c8
bouzuya <m@bouzuya.net>
2015-03-22 13:41:37 +0900

    add comment

--- src/cookie-storage.js
+++ src/cookie-storage.js
@@ -1,27 +1,41 @@
+// interface Storage {
+//   readonly attribute unsigned long length;
+//   [IndexGetter] DOMString key(in unsigned long index);
+//   [NameGetter] DOMString getItem(in DOMString key);
+//   [NameSetter] void setItem(in DOMString key, in DOMString data);
+//   [NameDeleter] void removeItem(in DOMString key);
+//   void clear();
+// };
 class CookieStorage {
   // TODO
   constructor() {
+    // readonly attribute unsigned long length;
     this.length = 0;
   }
 
   // TODO
-  key(index) {
+  // void clear();
+  clear() {
   }
 
   // TODO
+  // [NameGetter] DOMString getItem(in DOMString key);
   getItem(key) {
   }
 
   // TODO
-  setItem(key, data) {
+  // [IndexGetter] DOMString key(in unsigned long index);
+  key(index) {
   }
 
   // TODO
+  // [NameDeleter] void removeItem(in DOMString key);
   removeItem(key) {
   }
 
   // TODO
-  clear() {
+  // [NameSetter] void setItem(in DOMString key, in DOMString data);
+  setItem(key, data) {
   }
 }
 
