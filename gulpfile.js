4bb60d7a20b9a82112dd5f7380a036d90cb2dfd2
bouzuya <m@bouzuya.net>
2015-03-22 10:05:44 +0900

    add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build', function() {
   return gulp.src('./src/*')
@@ -7,5 +8,17 @@ gulp.task('build', function() {
     .pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('build-dev', function() {
+  return gulp.src('./src/*')
+    .pipe(sourcemaps.init())
+    .pipe(babel({ modules: "umd" }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/'));
+});
+
 gulp.task('default', ['build']);
+
+gulp.task('watch', ['build-dev'], function() {
+  gulp.watch('./src/*', ['build-dev']);
+});
 
