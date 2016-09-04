5278cc0ffef519ab8092f9620b0cfdc07784ad6a
bouzuya <m@bouzuya.net>
2016-09-04 15:04:13 +0900

    s/var/const/

--- src/cookie-storage.ts
+++ src/cookie-storage.ts
@@ -6,8 +6,9 @@
 //   [NameDeleter] void removeItem(in DOMString key);
 //   void clear();
 // };
+
 export class CookieStorage {
-  constructor(options) {
+  constructor(options: any) {
     options = options || {};
     this._defaultOptions = this._extends({
       path: null,
@@ -18,7 +19,7 @@ export class CookieStorage {
     // readonly attribute unsigned long length;
     Object.defineProperty(this, 'length', {
       get: () => {
-        var parsed = this._parse(document.cookie);
+        const parsed = this._parse(document.cookie);
         return Object.keys(parsed).length;
       }
     });
@@ -26,28 +27,28 @@ export class CookieStorage {
 
   // void clear();
   clear() {
-    var parsed = this._parse(document.cookie);
+    const parsed = this._parse(document.cookie);
     Object.keys(parsed).forEach(key => this.removeItem(key));
   }
 
   // [NameGetter] DOMString getItem(in DOMString key);
   getItem(key) {
-    var parsed = this._parse(document.cookie);
+    const parsed = this._parse(document.cookie);
     return parsed[key];
   }
 
   // [IndexGetter] DOMString key(in unsigned long index);
   key(index) {
-    var parsed = this._parse(document.cookie);
+    const parsed = this._parse(document.cookie);
     return Object.keys(parsed).sort()[index];
   }
 
   // [NameDeleter] void removeItem(in DOMString key);
   removeItem(key) {
-    var data = '';
-    var options = this._clone(this._defaultOptions);
+    const data = '';
+    const options = this._clone(this._defaultOptions);
     options.expires = new Date(0);
-    var formatted = this._format(key, data, options);
+    const formatted = this._format(key, data, options);
     document.cookie = formatted;
   }
 
@@ -55,12 +56,12 @@ export class CookieStorage {
   setItem(key, data, options) {
     options = options || {};
     options = this._extends(this._clone(this._defaultOptions), options);
-    var formatted = this._format(key, data, options);
+    const formatted = this._format(key, data, options);
     document.cookie = formatted;
   }
 
   _clone(o) {
-    var cloned = {};
+    const cloned = {};
     Object.keys(o).forEach(i => cloned[i] = o[i]);
     return cloned;
   }
@@ -94,12 +95,12 @@ export class CookieStorage {
 
   _parse(s) {
     if (!this._isDefined(s) || s.length === 0) return {};
-    var parsed = {};
-    var pattern = new RegExp('\\s*;\\s*');
+    const parsed = {};
+    const pattern = new RegExp('\\s*;\\s*');
     s.split(pattern).forEach((i) => {
-      var [encodedKey, encodedValue] = i.split('=');
-      var key = decodeURIComponent(encodedKey);
-      var value = decodeURIComponent(encodedValue);
+      const [encodedKey, encodedValue] = i.split('=');
+      const key = decodeURIComponent(encodedKey);
+      const value = decodeURIComponent(encodedValue);
       parsed[key] = value;
     });
     return parsed;
