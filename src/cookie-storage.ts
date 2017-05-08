7e57b1a54f05dd701914b99fad22459d27901b2c
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2017-05-07 23:44:46 -0700

    Initial implementation of [] accessors with the Proxy object.

--- src/cookie-storage.ts
+++ src/cookie-storage.ts
@@ -13,6 +13,7 @@ export class CookieStorage implements Storage {
       expires: null,
       secure: false
     }, defaultOptions);
+    return new Proxy(this, CookieStorageHandler);
   }
 
   get length(): number {
@@ -66,3 +67,85 @@ export class CookieStorage implements Storage {
   [key: string]: any;
   [index: number]: string;
 }
+
+var CookieStorageHandler: ProxyHandler<CookieStorage> = {
+    //getPrototypeOf? (target: T): object | null;
+        //Need to investigate
+    //setPrototypeOf? (target: T, v: any): boolean;
+        //Need to investigate
+    //isExtensible? (target: T): boolean;
+        //not necessary to implement. Calling Object.isExtensible(p); works correctly with no modifications on the proxy object.
+    //preventExtensions? (target: T): boolean;
+        //not necessary to implement. Calling Object.preventExtensions(p); works correctly with no modifications on the proxy object.
+    //getOwnPropertyDescriptor? (target: T, p: PropertyKey): PropertyDescriptor;
+        //not necessary to emulate LocalStorage and SessionStorage - both of those methods basically ignore propertyDescritors when setting. However, it seems to be nice!
+    getOwnPropertyDescriptor(target, p) {
+        return Object.getOwnPropertyDescriptor(target, p);
+    },
+    //has? (target: T, p: PropertyKey): boolean;
+    has(target, p) {
+        return target.getItem(p.toString()) ? true : false;
+    },
+    //get? (target: T, p: PropertyKey, receiver: any): any;
+    get(target, p) {
+        if (p in target) {
+            return target[p];
+        }
+        else {
+            let result = target.getItem(p.toString())
+            return result ? result : undefined;
+        }
+    },
+    //set? (target: T, p: PropertyKey, value: any, receiver: any): boolean;
+    set(target, p, value) {
+        let isExtensible = Object.isExtensible(target);
+        let alreadyExists = target.getItem(p.toString());
+        if (!isExtensible && alreadyExists) {
+            //"Attempting to add new properties to a non-extensible object will fail, either silently or by throwing a TypeError (most commonly, but not exclusively, when in strict mode)."
+            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/preventExtensions
+            throw new TypeError("Can't add property " + p.toString() + ", object is not extensible");
+        }
+        else {
+            target.setItem(p.toString(), value);
+            return true;
+        }
+    },
+    //deleteProperty? (target: T, p: PropertyKey): boolean;
+    deleteProperty(target, p) {
+        target.removeItem(p.toString());
+        return true;
+    },
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/handler/defineProperty
+    defineProperty(target, p, attributes) {
+        let isExtensible = Object.isExtensible(target);
+        let alreadyExists = target.getItem(p.toString());
+        //If the following invariants are violated, the proxy will throw a TypeError:
+        //--A property cannot be added, if the target object is not extensible."
+        //--A property cannot be added as or modified to be non-configurable, if it does not exists as a non-configurable own property of the target object.
+        //--A property may not be non-configurable, if a corresponding configurable property of the target object exists.
+        //--If a property has a corresponding target object property then Object.defineProperty(target, prop, descriptor) will not throw an exception.
+        //--In strict mode, a false return value from the defineProperty handler will throw a TypeError exception.
+        if (!isExtensible && alreadyExists) {
+            throw new TypeError("Can't add property " + p.toString() + ", object is not extensible");
+        }
+        else {
+            Object.defineProperty(target, p, attributes);
+            target.setItem(p.toString(), attributes.value);
+            return true;
+        }
+    },
+    //enumerate? (target: T): PropertyKey[];
+        //obsolete: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/handler/enumerate
+    //ownKeys? (target: T): PropertyKey[];
+    ownKeys(target) {
+        //todo: this code is duplicated from the private _getCookie() method on the CookieStorage object. I need to find the right pattern so you don't have to duplicate this private method in the proxy object.
+        //todo: I only added this line because I got a complier warning "target was declared but never used". Need to find a better way to remove this
+        console.log(target);
+        const parsed = parseCookies(document.cookie);
+        return Object.keys(parsed);
+    },
+    //apply? (target: T, thisArg: any, argArray?: any): any;
+        //not applicable to proxies for clasess, only proxies to functions
+    //construct? (target: T, argArray: any, newTarget?: any): object;
+        //overrides the new operator. Might not be necessary
+};
